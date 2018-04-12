5aea1972c3ab18629f1255dae00f91ab3c7bde5e
Wei Gao <gaow@seagroup.com>
2018-04-12 17:21:40 +0800

    hoc and container to render multiple and encapsulated children nodes

--- Container.js
+++ Container.js
@@ -2,8 +2,17 @@
 import React from 'react';
 
 const Container = props => {
-  const { children, ...rest } = props;
-  return <div>{React.cloneElement(children, {...rest})}</div>;
+  const {children, ...rest} = props;
+  const injectedProps = {containerInjected: 'y'};
+  return (
+    <div>
+      {children && children.length
+        ? children.map(child =>
+            React.cloneElement(child, {...injectedProps, ...rest}),
+          )
+        : React.cloneElement(children, {...injectedProps, ...rest})}
+    </div>
+  );
 };
 
 export default Container;
--- app.js
+++ app.js
@@ -3,20 +3,34 @@ import React from 'react';
 import withVariations from './withVariations';
 import Container from './Container';
 
-const UI = props => <div>{JSON.stringify(props)}</div>;
-const HocUI = withVariations(UI)({hocPropA: "x"});
+const UI = props => {
+  const {children, ...rest} = props;
+  return (
+    <div>
+      {JSON.stringify(rest)}
+      {...children}
+    </div>
+  );
+};
+const HocUI = withVariations(UI)({hocInjected: 'x'});
 
 const App = () => {
   return (
     <div>
       <div className="hoc-ui">
         <h1>HOC UI</h1>
-        <HocUI />
+        <HocUI additionalProp="hello">
+          <div>child</div>
+        </HocUI>
+        <div>sibling</div>
       </div>
       <div className="container-ui">
         <h1>Container UI</h1>
-        <Container containerPropA="x">
-          <UI />
+        <Container additionalProp="hello">
+          <UI>
+            <div>child</div>
+          </UI>
+          <div>sibling</div>
         </Container>
       </div>
     </div>
